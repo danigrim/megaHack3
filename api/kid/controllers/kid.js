378f01bfa5ac657d4a17367ec6d102513f39de19
danigrim <danigrim@stanford.edu>
2020-07-04 14:37:50 -0300

    configureAWS

--- api/kid/controllers/kid.js
+++ api/kid/controllers/kid.js
@@ -1,13 +1,24 @@
 'use strict';
+var distance = require('euclidean-distance')
+
 
 module.exports = {
 
-// friendList: async ctx => {
-//     const { kidId } = ctx.params
-//     const kid = await strapi.services.kid.findOne({ kidId });
-//     const friends = kid.friendList
-//     ctx.send(friends)
-// },
+  recommendBooks: async ctx => {
+       const { kidId } = ctx.params
+       const kid = await strapi.services.kid.findOne({ kidId });
+       const profile = kid.profile
+       const books = strapi.query.book
+       const b = books[0]
+       const bookProfile = b.profile.profile
+      // let dists = {}
+       //books.forEach((book) => {
+      //  dists[book._id] = distance(profile, book.profile)
+     //  })
+     console.log(profile)
+      let d = distance(profile.profile, bookProfile)
+       ctx.send({"data": d})
+   },
 
 // newFriend: async ctx => {
 //     const { kidId } = ctx.params
