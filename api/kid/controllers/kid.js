2ccaa640d86efa806d269c71d3bb87bc1b6037e2
danigrim <danigrim@stanford.edu>
2020-07-05 11:05:34 -0300

    fix/bugFixes&codeStandards

--- api/kid/controllers/kid.js
+++ api/kid/controllers/kid.js
@@ -1,5 +1,4 @@
 'use strict';
-//const bookRecommender = require('../services/kid')
 var distance = require('euclidean-distance')
 var natural = require('natural');
 
@@ -17,10 +16,8 @@ module.exports = {
     const kid = await strapi.services.kid.findOne({ kidId });
     const profile = kid.profile
     const kidAge = kid.ageRange
+    if(kidAge && profile && profile.profile){
     const books = await strapi.query('book').find({ageRange: kidAge})
-    if(kid.profile.profile == undefined){
-        ctx.send({error: 'error finding profile for kid'})
-    }
     let recs = {}
     const rank = []
     books.forEach((book)=> {
@@ -31,8 +28,10 @@ module.exports = {
         rank[i] = recs[dist]
     })
     ctx.send({"ranking": rank})
-
-    } catch (e) {
+    }else {
+        ctx.send({Error:"Faltando informação sobre a criança"})
+    }
+    }  catch (e) {
        ctx.send('error')
     }
 },
@@ -40,38 +39,48 @@ module.exports = {
 /**
  * Enpoint gera o vetor de cinco dimensões que define o perfil leitor do usuário baseado em uma média
  * do perfil dos cards selecionados pelo usuário.
- * @returns use reader profile
+ * @Params KidId - id do usuario, cardList - lista de Ids das cartas selecionadas pelo usuário
+ * @returns vetor definindo perfil do usuário [a, b, c, d, e]
  */
     makeProfile: async ctx => {
+        try{
         const { kidId } = ctx.params
         const  cardList  = ctx.request.body.cardlist
         const kid = await strapi.services.kid.findOne({kidId})
         if(!kid){
-            ctx.send('Essa criança não existe')
+            ctx.send({Error: "Essa criança não existe"})
         }
-        let kidProfile = kid.profile.profile? kid.profile.profile: [0, 0, 0, 0, 0]
-        let profileInstances = kid.profile.profileInstances ? (kid.profile.profileInstancescardList.length + cardList.length) : cardList.length
-        cardList.map(async (id) => {
+        //Se o usuário já passou por user profiling, começar a partir de valores anteriores, se não, um vetor neutro é inicializado para o usuário
+        let kidProfile = (kid.profile && kid.profile.profile) ? kid.profile.profile: [0, 0, 0, 0, 0]
+        const profileInstances = (Number(kid.numProfilings) > 0) ? (Number(kid.numProfilings) + cardList.length) : cardList.length
+        console.log('instances before', profileInstances)
+        const cardPromises = cardList.map(async (id, n) => {
             const card = await strapi.services.cards.findOne({ _id: id });
             if(card) {
                 const cardProfile = card.profile.profile
                 cardProfile.forEach((val, i)=>{
-                const val_sum = kidProfile[i] + val
-                kidProfile[i] = (val_sum) !== 0 ? (val_sum/profileInstances): 0
+                const profiles_read = profileInstances - (cardList.length - n -1)
+                const prev_val = kid.numProfilings ? Number(kidProfile[i])*(profiles_read-1) : 0
+                const val_sum = prev_val + val
+                kidProfile[i] = (profiles_read) !== 0 ? (val_sum/profiles_read): 0
             })
             }
         })
-        //update nymber of profilings!!
-        const kidupdate = await strapi.services.kid.update({ kidId }, {profile: {"profile": kidProfile}});
-        if(!kidupdate){
-            console.log('error in updating kid')
+        await Promise.all(cardPromises)
+        const profileInstancesUpdate = await strapi.services.kid.update({ kidId }, {numProfilings: profileInstances.toString()});
+        const profileUpdate = await strapi.services.kid.update({ kidId }, {profile: {"profile": kidProfile}});
+        if(!(profileInstancesUpdate && profileUpdate)){
+            ctx.send({Error: "Não foi possível atualizar o perfil da criança"})
         }
-        ctx.send({"updated kid": kidProfile})
+        ctx.send({"perfil Leitor calculado com sucesso!": kidProfile})
+    }catch(error){
+        ctx.send({error: error.message})
+    }
     },
 
     /**
-     * Endpoint recebe id de aluno, gera lista de palavras baseadas no livro indicado pelo professor
-     * reduzindo a lista para remover palavras parecidas/identicas.
+     * Endpoint recebe id de aluno, gera lista de palavras baseadas nos livros selecionados pelo professor
+     * em seu portal professor. A lista é reduzinda para remover palavras parecidas/identicas.
      * Remove todas as palavras com similaridade acima do nível 0.9 (como definido pelo padrão de JaronWinklerDistance)
      * @returns lista de palavras
      */
@@ -88,7 +97,7 @@ module.exports = {
       const bookPromises = bookIds.map(async (id)=> {
           const book = await strapi.query('book').findOne({_id: id})
           if (!book){
-              ctx.send('Livro não existe')
+              ctx.send({Error: 'Livro não existe'})
           }
             wordList = wordList.concat(tokenizer.tokenize(book.blurb))
       })
@@ -96,7 +105,9 @@ module.exports = {
       const reducedList = wordList.filter((word1, i) => {
         let unique = true
         for(var w = i+1; w<wordList.length; w++){
-          let wordProximity = natural.JaroWinklerDistance(word1.toLowerCase(), wordList[w].toLowerCase())
+          var first_word = word1.toLowerCase()
+          var second_word = wordList[w].toLowerCase()
+          let wordProximity = natural.JaroWinklerDistance(first_word, second_word)
           if(wordProximity > 0.90){
             unique = false
             break;
@@ -106,7 +117,7 @@ module.exports = {
      })
       ctx.send({palavras: reducedList})
     } catch(error) {
-            ctx.send({"error" :error})
+            ctx.send({"error" :error.message})
     }
     },
     /**
