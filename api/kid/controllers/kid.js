d5bbe4172d6746cbf569cf019af10a741fe8ceb3
danigrim <danigrim@stanford.edu>
2020-07-04 22:26:04 -0300

    merge

--- api/kid/controllers/kid.js
+++ api/kid/controllers/kid.js
@@ -109,15 +109,4 @@ module.exports = {
     }
     }
 
-
-// newFriend: async ctx => {
-//     const { kidId } = ctx.params
-//     const newFriend = ctx.request.body
-//     const kid = await strapi.services.kid.findOne({ kidId });
-//     const friends = kid.friendList
-//     friends['']
-//     ctx.send(friends)
-// }
-
-
 };
